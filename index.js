d0f55dbf40d829069aee9fdf9e30f0aa2a4a538b
Night Furry <ddhieu2707@gmail.com>
2025-11-20 22:44:28 +0700

    final?

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+let lang = "en"
+let currentLang = localStorage.getItem("lang")
 function select_language(language) {
+    lang = language
     $("[lang]").each(function () {
         if ($(this).attr("lang") == language)
             $(this).css("display", "block");
@@ -6,16 +9,29 @@ function select_language(language) {
             $(this).css("display", "none");
     });
 }
-function changeLanuage() {
+function changeLanuage(lang = currentLang) {
     let currentLang = $("#lang").text();
     if (currentLang == "EN") {
         select_language("vi")
         $("#lang").text("VN")
+        localStorage.clear()
+        localStorage.setItem("lang", 'vi')
     } else {
         select_language("en")
         $("#lang").text("EN")
+        localStorage.clear()
+        localStorage.setItem("lang", 'en')
     }
 }
 document.querySelector(".logo").addEventListener("click", function() {
     window.location.assign("/index.html")
 })
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed!");
+    select_language(currentLang)
+    if (currentLang == "vi") {
+        $("#lang").text('VN');
+    } else {
+        $("#lang").text('EN');
+    }
+});
